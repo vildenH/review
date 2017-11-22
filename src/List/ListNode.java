8c5fed0e9373e54696611f957ce0e4f4f87bc11d
wh <419957563@qq.com>
2017-11-23 00:37:52 +0800

    lengthOfLongestSubstring

--- src/List/ListNode.java
+++ src/List/ListNode.java
@@ -1,8 +1,9 @@
 package List;
 
+
 public class ListNode {
-    int val;
-    ListNode next = null;
+    public int val;
+    public ListNode next = null;
 
     ListNode(int val) {
         this.val = val;
--- src/test/Solution.java
+++ src/test/Solution.java
@@ -2,4 +2,18 @@ package test;
 
 public class Solution {
 
+    public void reOrderArray(int [] array) {
+        if(array==null){
+            return ;
+        }
+        for(int i=1;i<array.length;i++){
+            for(int j=i;j>0;j--){
+                if((array[j]&1)==1 && (array[j-1]&1)!=1){
+                    int temp=array[j];
+                    array[j]=array[j-1];
+                    array[j-1]=temp;
+                }
+            }
+        }
+    }
 }
